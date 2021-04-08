3a18bd0f608b7bae5c05a6ecad40c99b20e29623
suzinee <snoxox.py@gmail.com>
2021-04-08 22:27:23 +0900

    BOJ_17144: 미세먼지 안녕! rev3 정답이지만 timeout
    Signed-off-by: suzinee <snoxox.py@gmail.com>

--- air_pollution.py
+++ air_pollution.py
@@ -1,9 +1,5 @@
 import sys
-<<<<<<< HEAD
 from copy import deepcopy
-=======
-
->>>>>>> 45dc6b3e15701ce208120eeb7d7cba3f5c18b034
 sys.stdin = open('input.txt', 'r')
 
 # input
@@ -12,14 +8,7 @@ arr = []
 for _ in range(r):
     arr.append(list(map(int, input().split())))
 
-<<<<<<< HEAD
 tmp_arr = []
-=======
-#시간 초과의 이유로 사용하지 않음
-#dr = [0, 1, 0, -1]
-#dc = [1, 0, -1, 0]
-
->>>>>>> 45dc6b3e15701ce208120eeb7d7cba3f5c18b034
 pos_machine = []
 
 # system
@@ -46,60 +35,23 @@ def spread_dust(): #확산1
         for j in range(c):
             arr[i][j] += tmp_dust[i][j] # arr[i][j] = arr[i][j] + tmp_dust[i][j]
 
-# def spread_dust(): #확산2
-#     tmp_dust = [[0] * c for _ in range(r)]
-#     for i in range(r):
-#         for j in range(c):
-#             if arr[i][j] >= 5:
-#                 div5 = arr[i][j]//5
-#                 for dr, dc in (-1,0), (1,0), (0,1), (0,-1):
-#                     nr, nc = i + dr, j + dc
-#                     if 0 <= nr < r and 0 <= nc < c and arr[nr][nc] != -1:
-#                         tmp_dust[nr][nc] += div5
-#                         arr[i][j] -= div5
-#     for i in range(r):
-#         for j in range(c):
-#             arr[i][j] += tmp_dust[i][j]
+def run_machine(rr, cc, dirr):  # 공기청정기 작동
+    cp_arr = deepcopy(arr)
+    if dirr == -1:
+        rotate = ((0,1), (-1,0), (0,-1), (1,0))
+    else:
+        rotate = ((0,1), (1,0), (0,-1), (-1,0))
 
-<<<<<<< HEAD
-def run_machine(rr, cc, diff):  # 공기청정기 작동
-        cp_arr = deepcopy(arr)
-        dr = (1, -1, 0, 0)
-        dc = (0, 0, 1, -1)
-        for d in diff:
-            while True:  # 공기청정기의 오른쪽 값부터 시작해서 공기청정기를 만나면 종료한다.
-                nr, nc = rr + dr[d], cc + dc[d]
-                if -1 < nr < r and -1 < nc < c and arr[nr][nc] == -1:
-                    return cp_arr
-                if -1 < nr < r and -1 < nc < c:
-                    #print(nr, nc)
-                    cp_arr[nr][nc] = arr[rr][cc]
-                else:
-                    break
-                rr, cc = nr, nc
-                #print(cp_arr)
-=======
-def run_machine(start, dir):
-    if dir == -1: # 공기청소기 동작 - 반시계
-        for i in range(start - 1, 0, -1):
-            arr[i][0] = arr[i-1][0]
-        for j in range(0, c-1):
-            arr[0][j] = arr[0][j+1]
-        for i in range(0, start):
-            arr[i][c-1] = arr[i+1][c-1]
-        for j in range(c-1, 1, -1):
-            arr[start][j] = arr[start][j-1]
-    else: # 공기청소기 동작 - 시계
-        for i in range(start + 1, r - 1):
-            arr[i][0] = arr[i + 1][0]
-        for j in range(0, c - 1):
-            arr[r - 1][j] = arr[r - 1][j + 1]
-        for i in range(r - 1, start, -1):
-            arr[i][c - 1] = arr[i - 1][c - 1]
-        for j in range(c - 1, 1, -1):
-            arr[start][j] = arr[start][j - 1]
-    arr[start][1] = 0
->>>>>>> 45dc6b3e15701ce208120eeb7d7cba3f5c18b034
+    for dr, dc in rotate:
+        for _ in range(2*(r+c)-2):
+            nr, nc = rr + dr, cc + dc
+            if -1 < nr < r and -1 < nc < c and arr[nr][nc] == -1:  # 공기청정기의 오른쪽 값부터 시작해서 공기청정기를 만나면 종료한다.
+                return cp_arr
+            if -1 < nr < r and -1 < nc < c:
+                cp_arr[nr][nc] = arr[rr][cc]
+            else:
+                break
+            rr, cc = nr, nc
 
 # main
 for _ in range(t):
@@ -108,24 +60,16 @@ for _ in range(t):
 
     # 공기청소기 동작 - 반시계
     print('반시계')
-<<<<<<< HEAD
     print(arr)
-    tmp_arr = run_machine(pos_machine[0], 1, (2, 1, 3, 0))
+    tmp_arr = run_machine(pos_machine[0], 1, -1)
     arr = deepcopy(tmp_arr)
     arr[pos_machine[0]][1] = 0
     print(arr)
     # 공기청소기 동작 - 시계
     print('시계')
-    tmp_arr = run_machine(pos_machine[1], 1, (2, 0, 3, 1))
+    tmp_arr = run_machine(pos_machine[1], 1, 1)
     arr = deepcopy(tmp_arr)
     arr[pos_machine[1]][1] = 0
-=======
-    run_machine(pos_machine[0], -1)
-
-    # 공기청소기 동작 - 시계
-    print('시계')
-    run_machine(pos_machine[1], 1)
->>>>>>> 45dc6b3e15701ce208120eeb7d7cba3f5c18b034
 
 # output
 answer = sum(list(sum(arr, []))) + 2
