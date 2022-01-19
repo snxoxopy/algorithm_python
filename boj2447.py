af2788e4fd6250e8c0768c5383e9312819716d3d
snxoxopy <snoxox.py@gmail.com>
2022-01-19 16:16:56 +0900

    BOJ_2447: 별 찍기 - 10

--- boj2447.py
+++ boj2447.py
@@ -12,40 +12,56 @@ tags:
   - recursion
 ---
 
-문제이해: 분
+문제 이해: 분
 구현: 분
 Debug: 분
-참고자료
+참고 자료
 https://study-all-night.tistory.com/5
 """
 
 
 def draw_star(n):
-    global Map
+    global arr
 
+    # 종료 조건
+    # 3x3 단위 배열 초기 값 생성
     if n == 3:
-        Map[0][:3] = Map[2][:3] = [1] * 3
-        Map[1][:3] = [1, 0, 1]
+        arr[0][:3] = arr[2][:3] = [1] * 3
+        arr[1][:3] = [1, 0, 1]
         return
 
+    # 현 단계의 규칙 배열 크기 값을 반환
+    # N = 27 -> 9 -> 3
+    # a =  9 -> 3 -> 1
     a = n // 3
+
+    # 재귀 함수 호출
     draw_star(n // 3)
+
+    # 도장 찍기
+    # 1) 단위 배열 크기 반복
     for i in range(3):
         for j in range(3):
-            if i == 1 and j == 1:
-                continue
+            # 규칙 배열 중 가운데 배열을 초기 값으로 유지하기 위함
+            if i == 1 and j == 1: continue
+            # n = 3^i 일때는 가운데를 비워두고 "n = 3^(i-1) 일때의 별 배열" ; i > 1
+            # 2) 규칙 배열 크기 반복
             for k in range(a):
-                Map[a * i + k][a * j:a * (j + 1)] = Map[k][:a]  # 핵심 아이디어
+                arr[a * i + k][a * j:a * (j + 1)] = arr[k][:a]
 
-
+# input
 N = int(input())
 
-# 메인 데이터 선언
-Map = [[0 for i in range(N)] for i in range(N)]
+# system
+
+# arr 초기화
+arr = [[0 for i in range(N)] for i in range(N)]
 
 draw_star(N)
 
-for i in Map:
+# output
+# 별 찍기: 1 = * / 0 = ' '
+for i in arr:
     for j in i:
         if j:
             print('*', end='')
