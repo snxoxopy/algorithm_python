d3ad59d3e5c662475807e2e71bafe8516b27b3a4
snxoxopy <snoxox.py@gmail.com>
2022-01-28 21:21:20 +0900

    BOJ_1018: 체스판 다시 칠하기

--- boj1018.py
+++ boj1018.py
@@ -13,35 +13,38 @@ tags:
 ---
 
 문제 이해: 18분
-구현: 분
-Debug: 분
+구현: 25분
+Debug: 5분
 참고 자료
 https://god-gil.tistory.com/62
 """
 import sys
-sys.stdin = open("input.txt")
+sys.stdin = open("input.txt","r")
 
 n, m = map(int, sys.stdin.readline().split())
-# print(n, m)
-
 arr = [list(map(str, sys.stdin.readline())) for _ in range(n)]
+
 # print(arr)
-# print(arr[0][1])
-min_value = list()
+lst_min_colour = []
 
-for i in range(n - 7):      # 전체 행 탐색
-    for j in range(m - 7):  # 전체 열 탐색
-        wi, bi = 0, 0       # 2가지 경우의 수 배열 값 초기화
-        for k in range(i, i + 8):       # 8개 행 탐색
-            for l in range(j, j + 8):   # 8개 열 탐색
-                if (k + l) % 2 == 0:    # [경우 1) W / 경우 2) B] 가 있어야 할 위치
-                    if arr[k][l] != 'W': wi += 1
-                    else: bi += 1
-                else:                   # [경우 1) B / 경우 2) W] 가 있어야 할 위치
-                    if arr[k][l] != 'B': wi += 1
-                    else: bi += 1
-        min_value.append(wi)
-        min_value.append(bi)
-        #print(min_value)    # 바꿀 필요가 없다면, wi or bi 값이 증가하지 않으므로 0을 반환한다.
+# 문제 상황 이해
+# 상좌 검 or 상좌 흰 체스판의 경우 모든 체스판을 8x8 이동 탐색할 때
+# 64개인 경우는 0
+# 32개인 경우는 전부 한 가지 색으로 칠해진 경우
 
-print(min(min_value))
+for i in range(n - 7):
+    for j in range(m - 7):
+        cnt_b, cnt_w = 0, 0
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
+                if (k + l) % 2 == 0: # 짝수 번째 칸
+                    if arr[k][l] == 'W': cnt_w += 1 # 상좌 검 기준, 흰색이 나와야 함
+                    else: cnt_b += 1                # 상좌 흰 기준, 검은색이 나와야 함
+                else:                # 홀수 번째 칸
+                    if arr[k][l] == 'B': cnt_w += 1 # 상좌 검 기준, 흰색이 나와야 함
+                    else: cnt_b += 1                # 상좌 흰 기준, 검은색이 나와야 함
+        lst_min_colour.append(cnt_b)
+        lst_min_colour.append(cnt_w)
+
+
+print(min(lst_min_colour))
