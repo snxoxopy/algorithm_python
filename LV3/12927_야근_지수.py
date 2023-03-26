fb0f36ae09b3e9295de17a27b0292a4761439082
snxoxopy <snoxox.py@gmail.com>
2023-03-26 21:29:36 +0900

    [PRGRMS] 12927 야근 지수

--- LV3/12927_야근_지수.py
+++ LV3/12927_야근_지수.py
@@ -18,29 +18,32 @@ Debug: 60분
 참고 자료:
 
 """
+import heapq
+
+
 def solution(n, works):
     answer = 0
-    # 피로도 += 남은일 작업량^2 -> (시작한 시점 - 지금까지 한 일)^2
-    # 야근 피로도 최소
-    # N시간
-    # 남은일 작업량 works = []
-    # 최고 집합
-    works.sort()
-    hour = n
+    if sum(works) < n:
+        return answer
 
-    if n > sum(works):
-        return 0
-    else:
-        while hour != 0:
-            for i in range(len(works) - 1, -1, -1):
-                works[i] -= 1
-                hour -= 1
-                if hour == 0: break
-            # else: works[i] -= 1
+    for i in range(len(works)):
+        works[i] *= -1
 
-            # print(works)
+    heapq.heapify(works)
 
-        for task in works:
-            answer += (task) ** 2
+    for i in range(n):
+        # 작은 값 부터 반환
+        m = heapq.heappop(works)
+        # -1 보다 클 경우 이미 끝난 일
+        if m > -1:
+            heapq.heappush(works, m)
+        # 1시간동안 작업량 1처리
+        m += 1  # 음수처리했으므로 +1
+        heapq.heappush(works, m)
+
+    for _ in range(len(works)):
+        task = heapq.heappop(works)
+        # 음수를 곱하면 양수가 되므로 추가처리 불필요
+        answer += task ** 2
 
     return answer
