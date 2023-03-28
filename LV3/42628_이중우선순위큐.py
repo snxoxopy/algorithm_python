0a48fd6c68c40a96b83216ff9171cb96d170efc9
snxoxopy <snoxox.py@gmail.com>
2023-03-28 22:45:05 +0900

    [PRGRMS] 이중우선순위큐 최적화heapq.heappush

--- LV3/42628_이중우선순위큐.py
+++ LV3/42628_이중우선순위큐.py
@@ -16,13 +16,12 @@ tags:
 구현: 35분
 Debug: 15분
 참고자료
+https://school.programmers.co.kr/learn/courses/30/lessons/42628/solution_groups?language=python3
 """
 
 import heapq
 
-
 def solution(operations):
-    answer = [0, 0]
     arr = []
     q = []
     for strr in operations:
@@ -31,23 +30,18 @@ def solution(operations):
     for oprt, oprnd in arr:
         num = int(oprnd)
         if oprt == 'I':
-            q.append(num)
-            heapq.heapify(q)
-        else:
-            # print(num)
-            if len(q) == 0:
-                continue
-            else:
-                if num == -1:  # 최소값 삭제
-                    heapq.heappop(q)
-                elif num == 1:  # 최대값 삭제
-                    for i in range(len(q)):
-                        q[i] *= -1
-                    heapq.heapify(q)
-                    heapq.heappop(q)
-                    for i in range(len(q)):
-                        q[i] *= -1
-                    heapq.heapify(q)
+            heapq.heappush(q, num)
+        elif len(q) > 0:
+            if num == -1:  # 최소값 삭제
+                heapq.heappop(q)
+            elif num == 1:  # 최대값 삭제
+                for i in range(len(q)):
+                    q[i] *= -1
+                heapq.heapify(q)
+                heapq.heappop(q)
+                for i in range(len(q)):
+                    q[i] *= -1
+                heapq.heapify(q)
 
     if len(q) > 0:
         answer = [max(q), min(q)]
@@ -55,3 +49,13 @@ def solution(operations):
         answer = [0, 0]
 
     return answer
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.03ms, 10.4MB)
+테스트 2 〉	통과 (0.02ms, 10.1MB)
+테스트 3 〉	통과 (0.03ms, 10.2MB)
+테스트 4 〉	통과 (0.02ms, 10.4MB)
+테스트 5 〉	통과 (0.03ms, 10.2MB)
+테스트 6 〉	통과 (0.03ms, 10.2MB)
+"""
