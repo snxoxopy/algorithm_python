236d9588ef9b6a94e7dcedaf218ae4b0b1047821
snxoxopy <snoxox.py@gmail.com>
2023-04-05 07:57:29 +0900

    [PRGRMS] 가장 긴 팰린드롬 효율성 테스트 -> DP 구현으로 통과

--- LV3/12904_가장_긴_팰린드롬.py
+++ LV3/12904_가장_긴_팰린드롬.py
@@ -9,31 +9,47 @@ categories:
   - algorithm
 tags:
   - PROGRAMMERS
-  - Implementation
+  - DP
 ---
 
 문제 이해: 3분
 구현: 70분
 Debug: 분
 참고 자료:
+https://velog.io/@hyunjong96/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%ED%8C%B0%EB%A6%B0%EB%93%9C%EB%A1%AC
 """
+
 
 def solution(s):
     answer = 0
+    len_s = len(s)
+    # dp = [[0]*len_s]*len_s
+    dp = [[0 for _ in range(len_s)] for _ in range(len_s)]
+    # print(dp)
 
-    def isPalindrome(s):
-        str_s = ""
-        for i in range(len(s) - 1, -1, -1):
-            str_s += s[i]
-        # print(s, str_s)
-        if s == str_s:
-            return True
-        return False
+    for i in range(len_s):
+        # print(i)
+        dp[i][i] = 1
+        # print(dp)
+        answer = 1
+    # print(dp)
 
-    for diff in range(len(s), -1, -1):
-        for start_s in range((s)):
-            if(isPalindrome(s[start_s:start_s+diff])):
-                answer = max(answer, len(s[start_s:start_s+diff]))
-                break
+    for i in range(len_s - 1):
+        if (s[i] == s[i + 1]):
+            # print(s[i], s[i+1])
+            dp[i][i + 1] = 1
 
-        return answer
+    # print(dp)
+
+    for width in range(3, len_s + 1):
+        # print(width)
+        for i in range(len_s + 1):
+            if i + width == (len_s + 1): break
+            j = i + width - 1
+            # print("width, i, j = ", width, i, j)
+
+            if (s[i] == s[j] and dp[i + 1][j - 1]):
+                dp[i][j] = 1
+                answer = width
+
+    return answer
